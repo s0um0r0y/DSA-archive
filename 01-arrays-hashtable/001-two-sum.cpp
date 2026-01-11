f0840a0baeefa944b8a80090671415d725ea01f7
s0um0r0y <soumoroy09@gmail.com>
2026-01-11 12:25:40 +0530

    2 sum cpp solution

---
+++ 01-arrays-hashtable/001-two-sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for(int i = 0; i< nums.size(); i++){
+            for(int j = 0; j< nums.size(); j++){
+                if(i==j){
+                    break;
+                }
+                if(target==nums[i]+nums[j]){
+                    return {i,j};
+                }
+            }
+        }
+    return {};  
+    }
+};
