c3114a191a8a8afc2d871b1dca6e63a7acc170a9
s0um0r0y <soumoroy09@gmail.com>
2026-01-16 01:58:11 +0530

    solution for long numbers with limit bounds

---
+++ 01-arrays-hashtable/007-reverse-long-numbers.cpp
@@ -0,0 +1,22 @@
+#include <climits>
+class Solution {
+public:
+    int reverse(int x) {
+        long long revNum = 0;
+        while(x != 0){
+            int lastDigit = x%10;
+
+            if(revNum > INT_MAX /10 || (revNum == INT_MAX / 10 && lastDigit > 7)){
+                return 0;
+            }
+
+            if(revNum < INT_MIN /10 || (revNum == INT_MIN / 10 && lastDigit < -8)){
+                return 0;
+            }
+
+            revNum = revNum*10 + lastDigit;
+            x = x / 10;
+        }
+        return (int)revNum;
+        }
+};
